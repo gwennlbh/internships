120a5f7a8521a9637e152f8d7039561a1ad579aa
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-10 12:55:22 +0200

    Continue rapport

---
+++ autopush.fish
@@ -0,0 +1,7 @@
+while true
+	git add rapport/*.typ rapport/main.pdf bib.yaml *.fish
+	git commit --quiet -m "Continue rapport"
+	git push --quiet --force
+	echo Pushed at (date)
+	sleep (math "60 * 30")
+end
