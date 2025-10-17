64754f6410e806b24c9095cc9e245ca20997ee79
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-17 16:27:48 +0200

    perf: move pages_count computation away from CI into autopush.fish
    cuz apt-update in CI is soooooooooooo slow
    
    Signed-off-by: Gwenn Le Bihan <gwenn.lebihan7@gmail.com>

--- autopush.fish
+++ autopush.fish
@@ -2,7 +2,8 @@
 git reset --hard rapport/main.pdf
 git pull --rebase --autostash
 while true
-	git add rapport/*.typ bib.yaml *.fish rapport/*.dot
+	pdfinfo rapport/main.pdf | rg ^Pages: | awk '{print $2}' > pages_count
+	git add rapport/*.typ bib.yaml *.fish rapport/*.dot pages_count
 	git commit --quiet -m "Continue rapport"
 	git push --quiet --force
 	echo Pushed at (date)
