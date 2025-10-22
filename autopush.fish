a114d0e4da9fcd6e731aec76765cf248f7cc31f0
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-22 16:49:48 +0200

    Continue rapport

--- autopush.fish
+++ autopush.fish
@@ -3,7 +3,7 @@ git checkout -- rapport/main.pdf
 git pull --rebase --autostash
 while true
 	pdfinfo rapport/main.pdf | rg ^Pages: | awk '{print $2}' > pages_count
-	git add rapport/*.typ bib.yaml *.fish rapport/*.dot pages_count
+	git add rapport/*.typ bib.yaml *.fish rapport/*.dot pages_count rapport/*.png
 	git commit --quiet -m "Continue rapport"
 	git push --quiet --force
 	echo Pushed at (date)
