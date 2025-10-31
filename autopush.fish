52a96e666588d5cc47873d43f307f1a20d6f0c30
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-10-31 11:21:45 +0100

    Continue rapport

--- autopush.fish
+++ autopush.fish
@@ -3,7 +3,8 @@ git checkout -- rapport/main.pdf
 git pull --rebase --autostash
 while true
 	pdfinfo rapport/main.pdf | rg ^Pages: | awk '{print $2}' > pages_count
-	set pdf_changes (git diff --exit-code rapport/*.{typ,dot,png} bib.yaml; echo $status)
+	set pdf_changes (git diff --no-patch --exit-code rapport/*.{typ,dot,png} bib.yaml; echo $status)
+	echo "PDF updates with these changes: $pdf_changes"
 	git add rapport/*.typ bib.yaml *.fish rapport/*.dot pages_count rapport/*.png
 	git commit --quiet -m "Continue rapport"
 	if test $pdf_changes -ne 0
