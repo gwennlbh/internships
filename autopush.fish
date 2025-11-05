777b47d98a4a18628735ce8f34f1afbbde7ddaa7
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-11-05 15:51:34 +0100

    Start slides!
    Signed-off-by: Gwenn Le Bihan <gwenn.lebihan7@gmail.com>

--- autopush.fish
+++ autopush.fish
@@ -3,9 +3,9 @@ git checkout -- rapport/main.pdf
 git pull --rebase --autostash
 while true
 	pdfinfo rapport/main.pdf | rg ^Pages: | awk '{print $2}' > pages_count
-	set pdf_changes (git diff --no-patch --exit-code rapport/*.{typ,dot,png} bib.yaml; echo $status)
+	set pdf_changes (git diff --no-patch --exit-code {slides,rapport}/*.{typ,dot,png} bib.yaml; echo $status)
 	echo "PDF updates with these changes: $pdf_changes"
-	git add rapport/*.typ bib.yaml *.fish rapport/*.dot pages_count rapport/*.png
+	git add {rapport,slides}/*.{typ,dot,png} bib.yaml *.fish pages_count 
 	git commit --quiet -m "Continue rapport"
 	if test $pdf_changes -ne 0
 		git push --quiet --force
---
+++ slides/main.typ
@@ -0,0 +1,12 @@
+#import "../rapport/utils.typ": dontbreak
+#import "@preview/touying:0.6.1": *
+#import themes.simple: *
+
+#show: simple-theme.with(aspect-ratio: "16-9")
+#set text(font: "New Computer Modern")
+#show raw: set text(font: "Martian Mono", size: 0.8em)
+
+= _gz-unitree_: Reinforcement learning en robotique avec validation par moteurs de physique multiples pour le robot _H1v2_ d'Unitree
+
+Gwenn Le Bihan `<gwenn.lebihan@etu.inp-n7.fr>` \
+#datetime.today().display("[day padding:none] Novembre [year]") \
